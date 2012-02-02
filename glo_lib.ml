f671e8f5108c5c79b26d885b04b4f15fdd02864a
David Sheets <sheets@ashimaarts.com>
2012-02-02 08:21:01 +0800

    glom model clean-up, xml generation, begin gloxml->html xslt for glosse in-browser ide

--- glo_lib.ml
+++ glo_lib.ml
@@ -8,118 +8,49 @@ open Yojson
 
 include Glo_t
 
+type glom =
+  | Glo of glo
+  | Glom of (string * glom) list
+  | Source of string
+  | Other of Yojson.Safe.json
+
+exception UnserializableGlom of string
+exception InvalidGlom of string
+exception InvalidGlo of string
+
 let glo_version = (0,1,0)
-(*
-let set_field fld value = function
-  | Object f_alist ->
-      Object (List.map
-		(fun (k,v) -> if k=fld then (k,value) else (k,v))
-		f_alist)
-  | x -> x
 
-(* additive identity maps *)
-let strip_zero_fields = function
-  | Object f_alist -> Object
-      (List.filter
-	 (function
-	    | (k, Object []) | (k, Array []) | (k, Null) -> false
-	    | _ -> true
-	 ) f_alist)
-  | x -> x
-let add_zero_fields fl = function
-  | Object f_alist -> Object
-      (List.fold_left
-	 (fun fal (k,z,m) ->
-	    if List.mem_assoc k fal
-	    then List.map
-	      (fun (fn,v) ->
-		 if fn=k then (fn,m v) else (fn,v)
-	      ) fal
-	    else (k,z)::fal
-	 ) f_alist fl)
-  | x -> x
+let rec json_of_glom = function
+  (* TODO: don't serialize! *)
+  | Glo glo -> Safe.from_string (Glo_j.string_of_glo glo)
+  | Glom glom ->
+    `List (List.map (fun (n,glom) -> `List [`String n; json_of_glom glom]) glom)
+  | Source s ->
+    raise (UnserializableGlom "Raw source has no canonical glom serialization")
+  | Other json -> json
 
-let json_of_meta meta = strip_zero_fields (json_of_meta meta)
-let json_of_u u = strip_zero_fields (json_of_u u)
+let glo_of_json = function
+  (* TODO: don't serialize! *)
+  | `Assoc al -> Glo_j.glo_of_string (Safe.to_string ~std:true (`Assoc al))
+  | _ -> raise (Json_error "glo must be object")
 
-let json_of_glo glo =
-  let json = strip_zero_fields (json_of_glo glo) in
-  let json = match glo.meta with
-    | Some meta -> set_field "meta" (json_of_meta meta) json
-    | None -> json
-  in set_field "units"
-       (Array (Array.to_list (Array.map json_of_u glo.units)))
-       json
-*)
-let json_of_glom glom =
-  let rec group prefix prev = function
-    | ((_::[],_)::_) as glom -> (glom, List.rev prev)
-    | (x::xs,glo)::r when x=prefix -> group prefix ((xs,glo)::prev) r
-    | glom -> (glom,List.rev prev)
-  in
-  let rec nest prev = function
-    | [] -> `List (List.rev prev)
-    | (([],glo)::r) ->
-      nest ((`List [`String "";
-		    Safe.from_string (Glo_j.string_of_glo glo)])::prev) r
-    | ((x::[],glo)::r) ->
-      nest ((`List [`String x;
-		    Safe.from_string (Glo_j.string_of_glo glo)])::prev) r
-    | ((x::_,_)::_) as r -> let (rest,g) = group x [] r in
-	nest ((`List [`String x; nest [] g])::prev) rest
-  in let split s = Str.split (Str.regexp_string "/") s in
-    nest [] (List.map (fun (n,glo) -> (split n, glo)) glom)
-(*
-let string_of_glo ?(compact=true) glo =
-  Json_io.string_of_json ~compact (json_of_glo glo)
-let string_of_glom ?(compact=true) glom =
-  Json_io.string_of_json ~compact (json_of_glom glom)
+let rec glom_of_json = function
+  | `List jl -> Glom
+    (List.map
+       (function
+	 | `List [`String n; (`List _) as v]
+	 | `List [`String n; (`Assoc _) as v] ->
+	   (n, (glom_of_json v))
+	 | _ ->
+	   raise (InvalidGlom "glom array type must be (string * (glo|glom)) list")
+       ) jl)
+  | `Assoc al -> Glo (glo_of_json (`Assoc al))
+  | _ -> raise (Json_error "expected glom array or glo object")
 
-let glo_of_json json = let id x = x in
-let json = add_zero_fields [
-  "meta",Null,
-  (add_zero_fields [
-     "author",Array [],id;
-     "license",Null,id;
-     "library",Null,id;
-     "version",Null,id;
-     "build",Null,id;
-   ]);
-  "units",Array [],
-  (function
-     | Array l ->
-	 Array (List.map
-		  (add_zero_fields [
-		     "pdir",Array [],id;
-		     "edir",Object [],id;
-		     "vdir",Null,id;
-		     "insym",Array [],id;
-		     "outsym",Array [],id;
-		     "inmac",Array [],id;
-		     "opmac",Array [],id;
-		     "outmac",Array [],id;
-		   ]) l)
-     | x -> x
-  );
-  "linkmap",Object [],id;
-] json in (*try*) glo_of_json json
-(*  with e -> (print_endline (Json_io.string_of_json json);
-	     print_endline (Printexc.to_string e);
-	     Printexc.print_backtrace stdout;
-	     exit 127)
-*)
-*)
-let glom_of_json json =
-  let rec flat prefix = function
-    | `List jl -> List.fold_left
-      (fun p -> function
-	| `List [`String n; `Assoc al] ->
-	  (prefix^n,
-	   Glo_j.glo_of_string (Safe.to_string ~std:true (`Assoc al)))::p
-	| `List [`String n; `List al] ->
-	  (flat (prefix^n^"/") (`List al))@p
-	| _ ->
-	  raise (Json_error "glom array elements must be string * (glo|glom)")
-      ) [] jl
-    | _ -> raise (Json_error "glom must be array")
-  in List.rev (flat "" json)
+let glom_of_string s =
+  try glom_of_json (Glo_j.glom_of_string s)
+  with Json_error _ | Failure _ -> Source s
+
+let string_of_version (maj,min,rev) =
+  let soi = string_of_int in
+  (soi maj)^"."^(soi min)^"."^(soi rev)
---
+++ glosse.xsl
@@ -0,0 +1,99 @@
+<?xml version="1.0"?>
+
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+                xmlns="http://www.w3.org/TR/xhtml1/strict">
+
+  <xsl:output method="html" />
+
+  <xsl:template name="glol_params">
+    <h2>glol</h2>
+    <form>
+      <ul>
+        <li><input type="text" /></li>
+      </ul>
+    </form>
+  </xsl:template>
+
+  <xsl:template name="glom_name">
+    <xsl:if test="@name">
+      <h3><xsl:value-of select="@name" /></h3>
+    </xsl:if>
+  </xsl:template>
+
+  <xsl:template name="glo_name">
+    <xsl:choose>
+      <xsl:when test="@name"><xsl:value-of select="@name" /></xsl:when>
+      <xsl:otherwise>[unknown]</xsl:otherwise>
+    </xsl:choose>
+  </xsl:template>
+
+  <xsl:template match="/">
+    <html>
+      <head>
+        <title>glosse : glo shader system editor</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+
+        <link rel="stylesheet" type="text/css" href="glosse.css" />
+        <link rel="stylesheet" type="text/css" href="../CodeMirror2/lib/codemirror.css" />
+        <script src="../CodeMirror2/lib/codemirror.js"></script>
+        <script src="../CodeMirror2/mode/clike/clike.js"></script>
+        <script src="../CodeMirror2/keymap/emacs.js"></script>
+      </head>
+      <body>
+        <xsl:choose>
+          <xsl:when test="glom">
+            <div id="sidebar">
+              <div id="glol">
+                <xsl:call-template name="glol_params" />
+              </div>
+              <div id="glom">
+                <h2>glom</h2>
+                <xsl:apply-templates mode="glom">
+                  <xsl:with-param name="path" />
+                </xsl:apply-templates>
+              </div>
+            </div>
+            <div id="gloc">
+              <form>
+                <textarea id="source" name="source">
+                </textarea>
+              </form>
+              <div id="stderr">
+                stderr
+              </div>
+              <div id="stdout">
+                stdout
+              </div>
+            </div>
+          </xsl:when>
+          <xsl:otherwise>
+            <xsl:call-template name="format_error" />
+          </xsl:otherwise>
+        </xsl:choose>
+        <script type="text/javascript">
+          var editor = CodeMirror.fromTextArea(document.getElementById("source"), {
+            lineNumbers: true,
+            mode: "text/x-csrc",
+            keyMap: "emacs"
+          });
+        </script>
+      </body>
+    </html>
+  </xsl:template>
+
+  <xsl:template match="glom" mode="glom">
+    <xsl:param name="path" />
+    <xsl:call-template name="glom_name" />
+    <ul>
+      <xsl:for-each select="child::*">
+        <li><xsl:apply-templates select="." mode="glom" /></li>
+      </xsl:for-each>
+    </ul>
+  </xsl:template>
+
+  <xsl:template match="glo" mode="glom">
+    <a href="#"><xsl:call-template name="glo_name" /></a>
+  </xsl:template>
+</xsl:stylesheet>
