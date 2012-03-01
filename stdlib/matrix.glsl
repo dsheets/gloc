2b13844a201e2dd8ced246272084c16c2862f46c
Ian McEwan <ijm@ashimaarts.com>
2012-03-01 18:38:31 +0800

    fixed typos.

--- stdlib/matrix.glsl
+++ stdlib/matrix.glsl
@@ -20,15 +20,15 @@ mat2 transpose(mat2 m) {
   }
 
 mat3 transpose(mat3 m) {
-  return mat3(  m[0][0], m[1][0], m[2][0]   // new col 0
-                m[0][1], m[1][1], m[2][1]    // new col 1
-                m[0][2], m[1][2], m[2][2]    // new col 1
+  return mat3(  m[0][0], m[1][0], m[2][0],  // new col 0
+                m[0][1], m[1][1], m[2][1],  // new col 1
+                m[0][2], m[1][2], m[2][2]   // new col 1
              );
   }
 
 mat4 transpose(mat4 m) {
-  return mat4(  m[0][0], m[1][0], m[2][0], m[3][0]   // new col 0
-                m[0][1], m[1][1], m[2][1], m[3][1]    // new col 1
+  return mat4(  m[0][0], m[1][0], m[2][0], m[3][0],   // new col 0
+                m[0][1], m[1][1], m[2][1], m[3][1],    // new col 1
                 m[0][2], m[1][2], m[2][2], m[3][2]    // new col 1
              );
   }
