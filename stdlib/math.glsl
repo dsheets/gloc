43c5798115d92eae4a8f8ad25866119160fc49d4
Ian McEwan <ijm@ashimaarts.com>
2012-03-01 15:02:17 +0800

    more math, matrix functions

--- stdlib/math.glsl
+++ stdlib/math.glsl
@@ -63,4 +63,59 @@ ATANH(vec2)
 ATANH(vec3)
 ATANH(vec4)
 
+// Need to find a neater way of doing this without going via ints.
+#define TRUNC(T) \
+  T trunc(T x) {\
+    return sign(x)*floor(abs(x)); \
+    }
+
+TRUNC(float)
+TRUNC(vec2)
+TRUNC(vec3)
+TRUNC(vec4)
+
+#define ROUND(T) \
+  T round(T x) {\
+    return floor(x + 0.5); \
+  }
+
+ROUND(float)
+ROUND(vec2)
+ROUND(vec3)
+ROUND(vec4)
+
+// RoundEven 
+#define ROUNDEVEN(T) \
+  T roundEven(T x) { \
+    T y = floor(x + 0.5); \
+    return ( y - x == 0.5 ) ? floor(0.5*y)*2.0 : y \
+    }
+     
+ROUNDEVEN(float)
+ROUNDEVEN(vec2)
+ROUNDEVEN(vec3)
+ROUNDEVEN(vec4)
+      
+
+#define FMA(T) \
+  T fma(T a, T b, T c) { \
+    return  a*b + c; \
+    }
+
+FMA(float)
+FMA(vec2)
+FMA(vec3)
+FMA(vec4)
+
+#define MODF(T) \
+  T modf(T x, out T i) {\
+    i = floor(x); \
+    return x - i; \
+    } 
+
+MODF(float)
+MODF(vec2)
+MODF(vec3)
+MODF(vec4)
+
 
