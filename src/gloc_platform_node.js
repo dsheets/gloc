d79b15412945ffe9014bc774f44225db07886298
Tikhon Jelvis <tikhon@berkeley.edu>
2012-05-23 12:24:55 -0700

    Made the external functions in gloc_platform_node global so they work with the required modules.

--- src/gloc_platform_node.js
+++ src/gloc_platform_node.js
@@ -2,35 +2,39 @@ var fs = require('fs');
 
 var ocaml = {};
 
-function caml_sys_getenv(name) {
+global.caml_sys_getenv = function (name) {
   return process.env[name];
-}
+};
 
-var register_ocaml_fn = (function (ocaml) {
+global.register_ocaml_fn = (function (ocaml) {
   return function (name, fn) {
     ocaml[name] = fn;
   };
 })(ocaml);
 
-function gloc_stdout(msg) {
+global.gloc_stdout = function (msg) {
   process.stdout.write(msg);
-}
+};
 
-function gloc_stderr(msg) {
+global.gloc_stderr = function (msg) {
   process.stderr.write(msg);
-}
+};
 
 // Node does not seem to support reading from STDIN synchronously
-function gloc_stdin() {  
-  throw "Input from STDIN is not supported under Node JS.";
-}
+global.gloc_stdin = function () {  
+  throw "Input from STDIN is currently not supported under Node JS.\n";
+};
 
 // I'm assuming all files are in utf8
-function gloc_fs_read(path) {
+global.gloc_fs_read = function (path) {
   return fs.readFileSync(path, "utf8");
-}
+};
 
-function gloc_fs_write(path, content) {
+global.gloc_fs_write = function (path, content) {
   fs.writeFileSync(path, content);
-}
+};
 
+require('./_build/gloc_js.d.js');
+
+// The first two arguments are the path to node and your script respectively:
+ocaml.gloc(process.argv.slice(2)); 
